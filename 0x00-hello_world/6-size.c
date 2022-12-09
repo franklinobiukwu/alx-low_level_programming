ae5e29539fdd4783e86ac7fadf9374000b45a758
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-08 21:29:31 -0800

    Size is not granduer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 /**
- *	main - Entry point
+ * main - Entry point
  *
- *	Description: 'size is not grandeur'
+ * Return: Always 0 (Success)
  *
- *	Return: Always 0 (Success)
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longType;
-	long long int longLongIntType;
-	float floatType;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %lu byte(s)\n", sizeof(longType));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
-	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
-
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
