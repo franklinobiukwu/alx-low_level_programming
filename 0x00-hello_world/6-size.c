589822ec9def5e231b57c9f8191364087998038b
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-08 18:37:38 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,17 @@
  */
 int main(void)
 {
-	int intType;
 	char charType;
+	int intType;
+	long int longType;
+	long long int longLongIntType;
 	float floatType;
-	double doubleType;
 
+	printf("Size of char type is %lu bytes\n", sizeof(charType));
 	printf("Size of integer type is %lu bytes\n", sizeof(intType));
-	printf("Size of integer type is %lu bytes\n", sizeof(charType));
-	printf("Size of integer type is %lu bytes\n", sizeof(floatType));
-	printf("Size of integer type is %lu bytes\n", sizeof(doubleType));
+	printf("Size of long int type is %lu bytes\n", sizeof(longType));
+	printf("Size of long long int type is %lu bytes\n", sizeof(longLongIntType));
+	printf("Size of float type is %lu bytes\n", sizeof(floatType));
 
 	return (0);
 }
