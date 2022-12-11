c9be7de3cbe342a7dfa9546f274e4cf7ef3a2f61
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-11 13:33:20 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *	Description: ''
+ *
+ *	Return: 0 (Success
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
