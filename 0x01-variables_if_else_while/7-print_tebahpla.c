1053ced8f4839a84150c8313354fb66594b17b25
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-11 14:27:55 -0800

    prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *	Description: ''
+ *
+ *	Return: (0) (Success)
+ */
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
