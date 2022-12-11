b3f2bf99df34ed7bf2fb892f4b64307386131205
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-11 11:24:22 -0800

    Print 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *	Description: 'Print numbers'
+ *
+ *	Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+
+	return (0);
+}
