d12c9e2b4b8b2a019bade7f49748046d6c685147
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-11 04:34:25 -0800

    Letters of the Alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *	Description: 'A program that prints the alphabet in lowercase'
+ *
+ *	Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
