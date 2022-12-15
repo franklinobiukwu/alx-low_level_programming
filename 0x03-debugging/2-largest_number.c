b1350ac5cbc289dbdfb4105477e6735800088ace
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-15 09:30:46 -0800

    updates

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
 	else
 	{
 		largest = c;
