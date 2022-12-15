4973e8bb3070654c25261b29be0f1a978efa9f1a
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-15 01:58:21 -0800

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *	add - add two integers and return result
+ *
+ *	@a: first member
+ *
+ *	@b: second member
+ *
+ *	Return: Always sum.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *	times_table - prints the 9 times table
+ *
+ *	return: void
+ */
+
+void times_table(void)
+{
+	int i, j;
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j)
+	}
+
+}
