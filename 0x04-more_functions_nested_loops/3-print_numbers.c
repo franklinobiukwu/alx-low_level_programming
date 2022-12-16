5726ecd073e0d024b3449d936bd911ea10d3943d
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-16 08:28:26 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9.
+ *
+ * Return: Void.
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n >= '9'; n++)
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
