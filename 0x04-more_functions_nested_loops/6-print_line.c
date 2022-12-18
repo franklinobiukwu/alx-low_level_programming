cd6010d7c9b4062ade45cd3ed0091c40174b22c2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-18 13:44:21 -0800

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,19 +12,19 @@ void print_line(int n)
 {
 	int l;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (l = 0; l <= n; l++)
 		{
-			if (n == 0)
+			/*if (n == 0)
 			{
 				_putchar('\n');
 				continue;
-			}
+			}*/
 			_putchar('_');
 		}
 	}
-	else if (n < 0)
+	else if (n <= 0)
 	{
 		_putchar('\n');
 	}
