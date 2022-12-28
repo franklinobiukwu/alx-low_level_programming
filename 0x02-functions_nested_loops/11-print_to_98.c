7971b5ad22e4c3bca76b911c90f20215e8a33252
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-27 20:27:22 -0800

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.c"
+/**
+ *	print_to_98 - print all natural numbers from n to 98
+ *
+ *	@n: member
+ *
+ *	Return: Always void
+ */
+
+void print_to_98(int n)
+{
+
+	int i;
+
+	if (n <= '98')
+	{
+		for (i = n; i <= '98'; i++)
+		{
+			_pu	
+		}
+	}
+
+}
