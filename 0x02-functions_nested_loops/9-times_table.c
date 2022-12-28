147b5783e44e4af1052b138e96266efbf606c453
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2022-12-28 11:22:30 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,30 @@
 
 void times_table(void)
 {
-	int i, j;
-	for (i = '0'; i <= '8'; i++)
+	int i, j, result, t, u;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (j)
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			t = ('0' + (result / 10));
+			u = ('0' + (result % 10));
+
+			if (t == '0')
+				_putchar (' ');
+			else
+				_putchar (t);
+			_putchar (u);
+
+			if (j != 9)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+
+		_putchar ('\n');
 	}
 
 }
