1a7f5be16238543a4adc758cb827f698a25243bc
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-17 15:10:36 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print a - z
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
