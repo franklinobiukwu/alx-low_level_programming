c92bb29884e7a59ffdc30d45d343803b75523543
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-21 14:17:29 -0800

    Print the alphabet in lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* void print_alphabet_x10(void) - Prints 10 times the alphabet in lowercase
+*
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; i <=10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(i);
+		}	
+
+		_putchar('\n');
+	}
+
+	return;
+}
