01527952e1cfceb5fd8e2f770b8982895dac04f4
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-21 14:53:04 -0800

    Alphabetic komo

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isalpha - Checks for alphabetic characters
+*
+* @c: first value
+*
+* Return: On success  1
+*/
+
+int _isalpha(int c)
+{
+	if (c == 'a' && c <= 'z' || c == 'A' and c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
