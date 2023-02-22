6bb51bb294709c1bd71db31b1f662020cd4f03b3
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-22 05:27:01 -0800

    prints the n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* print_times_table - Print the n times table
+* Return: void
+*/
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+
+				if (k > 99)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k > 9)
+                                {
+                                        _putchar(' ');
+                                        _putchar((k / 10) + '0');
+                                        _putchar((k % 10) + '0');
+                                }
+				else if (k <= 9 && j != 0)
+                                {
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(k + '0');
+                                }
+				else if (k == 0 && j == 0)
+                                        _putchar('0');
+
+				if (k != (n * n))
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
+			_putchar('\n');
+		}
+	_putchar('\n');
+	}
+}
