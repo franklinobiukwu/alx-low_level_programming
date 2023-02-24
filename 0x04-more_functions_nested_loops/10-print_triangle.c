905e053d55042c368a64bf820551ba4b91a84671
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-24 14:05:07 -0800

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle followed by a line
+*
+* @size: parameter
+*
+* Return: void
+*/
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	int size_up = 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < size_up; k++)
+				_putchar('#');
+			size_up++
+			_putchar('\n');
+		}
+	}
+}
