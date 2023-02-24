410a7148975d1994d29f29f242f24092a033745f
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-23 19:08:02 -0800

    checks for digit between 0 and 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isdigit - Checks for digit between 0 and 9
+*
+* @c: parameter
+*
+* Returns: 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
