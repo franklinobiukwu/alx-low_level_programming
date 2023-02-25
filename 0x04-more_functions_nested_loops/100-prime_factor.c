51a10cc746bc398a44ba1ac00a7662a13f1f24e4
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-24 20:09:47 -0800

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: find and print the largest prime factor of a number
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	long n = 612852475143;
+	long factor = 2;
+
+	while (n > 1)
+	{
+		if (n % factor == 0)
+		{
+			n /= factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+
+	printf("%ld", factor);
+
+	printf("\n");
+}
