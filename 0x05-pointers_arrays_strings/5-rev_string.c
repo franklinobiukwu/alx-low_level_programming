41b4e6e0076ba03e44705448900d6ec361a0d151
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-01 00:49:47 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string
+*
+* @s: parameter
+*
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i, strlen, temp, halfstr;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	strlen = i - 1;
+	halfstr = strlen / 2;
+
+	for (i = 0; i < halfstr; i++)
+	{
+		temp = s[i];
+		s[i] = s[strlen];
+		s[strlen] = temp;
+		strlen--;
+	}
+}
