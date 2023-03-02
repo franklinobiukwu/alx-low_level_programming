2700ed3604d2a57506e04752121670b601bb44ef
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-02 23:11:19 +0100

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\n'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (; i < n; i++)
-		dest[i] = '\n';
+	for (; n > i; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
