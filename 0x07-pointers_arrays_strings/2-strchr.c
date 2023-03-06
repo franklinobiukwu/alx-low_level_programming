726c0574f8d2ac02d8928cfed58a3cb37fd7a9e2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-06 22:18:53 +0100

    locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *ptr;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
-			return ((s + i));
+			ptr = (s + i);
+			return (ptr);
 		}
 
 	}
