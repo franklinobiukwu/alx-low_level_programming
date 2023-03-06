0daef6e1080d657752cedb0a86e4450aea11e496
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-07 00:59:02 +0100

    search string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,13 +12,17 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
-				return (s[i]);
+			{
+				p = &s[i];
+				return (p);
+			}
 		}
 	}
 
