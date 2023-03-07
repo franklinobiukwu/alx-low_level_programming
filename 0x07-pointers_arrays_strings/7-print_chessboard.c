304d2d8e33aae596bd03f729e8650e6d008f6a7b
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-07 04:13:38 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_chessboard - print chessboard
+*
+* @a: parameter
+*
+* Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar('\n');
+	}
+
+}
