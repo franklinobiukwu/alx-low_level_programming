8371533002a3b671e1744519a6f737466518487d
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-08 02:01:44 +0100

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,21 +14,21 @@ char *_strstr(char *haystack, char *needle)
 	int i, j, k;
 	int c = 0, a = 1;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[j] == needle[i])
+			if (haystack[i] == needle[j])
 			{
-				for (k = i; needle[k] >= '\0'; k++)
+				for (k = j; needle[k] >= '\0'; k++)
 				{
-					if (needle[k + a] == haystack[j + a])
+					if (needle[k + a] == haystack[i + a])
 					{
 						c++;
 						a++;
 					}
 					if (needle[k] == '\0')
-						return (&haystack[j]);
+						return (&haystack[i]);
 					continue;
 				}
 
