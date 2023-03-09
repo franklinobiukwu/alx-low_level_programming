7e5be659c6bdbe55927d1341d268a1f774037c34
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-09 02:44:09 +0100

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+* _puts_recursion - Prints a string, followed by a line
+*
+* @s: Pointer to string
+*
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
