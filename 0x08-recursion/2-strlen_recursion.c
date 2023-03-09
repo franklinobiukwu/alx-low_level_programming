87e541cad4cef6cd6660e03e6e107e06653d9cdf
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-09 13:22:15 +0100

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,13 +11,12 @@
 int _strlen_recursion(char *s)
 {
 
-	int n = 0;
+	int n;
 
 	if (*s == '\0')
 		return (0);
 
-	n++;
-	_strlen_recursion(s + 1);
+	n = 1 + _strlen_recursion(s + 1);
 
 	return (n);
 }
