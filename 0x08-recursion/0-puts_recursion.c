1b13a42e11abb6da435ed9571ea2795a3bcf1d34
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-09 03:15:16 +0100

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
+		_putchar('\n');
+	else
 	{
 		_putchar(*s);
-		s++;
+		_puts_recursion(++s);
 	}
-
-	_putchar('\n');
 }
