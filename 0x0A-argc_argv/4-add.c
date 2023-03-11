be33781eb60320345f0c37b72dfb6cb7f0039950
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-11 04:24:46 +0100

    sum positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,10 +15,10 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc == 1)
-		printf("0");
+		printf("0\n");
 	else
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (atoi(argv[i]) >= 0)
 				sum += atoi(argv[i]);
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		printf("%d", sum);
+		printf("%d\n", sum);
 	}
 
 	return (0);
