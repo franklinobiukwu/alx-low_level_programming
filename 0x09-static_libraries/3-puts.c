7b6be4aeaa8ff8b79d6c9542a2eb6b0f2e81b336
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-11 01:10:00 +0100

    static library libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _puts - Prints a string, followed by a new line
+*
+* @str: parameter
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	int v, i = 0;
+
+	while (str[i] != '\0')
+	{
+		v = str[i];
+		_putchar(v);
+		i++;
+	}
+
+	_putchar('\n');
+}
