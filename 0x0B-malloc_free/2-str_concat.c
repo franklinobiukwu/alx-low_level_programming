cbef9a7101fda27261328dc0bd69ea81f21a6d19
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-15 00:19:54 +0100

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, l, m, n;
 
 	/*return NULL if s1 or s2 is NULL*/
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	/*Determine the length of s1 and s2*/
 	for (i = 0; s1[i] != '\0'; i++)
