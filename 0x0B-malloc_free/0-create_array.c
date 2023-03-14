f4e0c2b9a9641476b9418ccd6e16e757a2e37b6e
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-14 18:19:35 +0100

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 * @size: size of memory
 * @c: value of memory
 *
-* Return: pointer to character
+* Return: pointer to charcter array on Success or NULL on Error
 */
 
 char *create_array(unsigned int size, char c)
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
