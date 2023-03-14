225d3ae7e5dd56076832c88382b3182a5bfbc853
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-14 18:12:40 +0100

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,16 +17,16 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-
-	arr = malloc(size * sizeof(char));
 	if (size == 0)
 		return (NULL);
+
+	arr = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
 	if (arr == NULL)
-		return (NULL);
+		return (0);
 
 	return (arr);
 }
