6049da40a5ed8195e64e792bf03477579bb6225e
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-15 19:23:06 +0100

    free 2d array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - free pointer to array
+*
+* @grid: pointer to array
+* @height: height of array
+*
+* Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	/*Free each row of grid*/
+	while (height > 0)
+	{
+		free(grid[i]);
+		i++;
+	}
+
+	/*Free grid*/
+	free(grid);
+}
