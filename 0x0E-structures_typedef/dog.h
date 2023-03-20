90c3dddcd15b7fb67cd34a247577ebf773ffbc5b
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-20 23:22:05 +0100

    define dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
+
+/**
+* struct dog - dog credentials
+*
+* @name: name of dog
+* @age: age of dog
+* @owner: dog owner parameter
+*
+* Return: 0 on success
+*/
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
+	/*dog credentials*/
 }
 #endif
