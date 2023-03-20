c162d4f4433af7f8ccab880987b2b3d29684977e
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-20 19:41:08 +0100

    print name of source file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: print the name of source file
+*
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
