8d685d9e474d349dcc1946be2edb1860866d47ab
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-21 05:59:40 +0100

    added standard library header

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdlib>
+#include <stdlib.h>
 #include "dog.h"
 /**
 * init_dog - initialize a variable of type struct dog
@@ -14,8 +14,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		p = malloc(sizeof(struct dog));
-	*d.name = name;
-	*d.age = age;
-	*d.owner = owner;
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
