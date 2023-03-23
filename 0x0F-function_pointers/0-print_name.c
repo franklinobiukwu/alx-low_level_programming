244b6951ec5f8404f04d02c7ca44664f9be6f43f
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-23 15:08:23 +0100

    print name CODE FIX #4

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -11,7 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
