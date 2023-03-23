3bb6bbfe6b777be2c80042136c1ad2d7276ea96f
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-23 22:00:45 +0100

    prints strings, followed by a new line CODE FIX #1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(strings, char *);
 		/*print 'nil' if string is NULL*/
 		if (s == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", s);
 		if (i < (n - 1) && separator != NULL)
