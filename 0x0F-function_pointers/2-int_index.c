6012d36c6821e3d30dd7c7bf4ef748775022e3b1
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-23 14:58:03 +0100

    searches for an integer CODE FIX #3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,19 +16,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 	/*variable to hold index*/
 	int index, h;
 
-	/*return 0 if array, size, or cmp is NULL*/
-	if (!array || !size || !cmp)
-		return (NULL);
 	if (size <= 0)
 		return (-1);
 	/*loop through elements of array and set each element as input to cmp*/
-	for (index = 0; index < size; index++)
+	if (array && size && cmp)
 	{
-		h = (*cmp)(array[index]);
-		/*return index if function returns true*/
-		if (h != 0)
+		for (index = 0; index < size; index++)
 		{
-			return (index);
+			h = (cmp)(array[index]);
+			/*return index if function returns true*/
+			if (h != 0)
+			{
+				return (index);
+			}
 		}
 	}
 
