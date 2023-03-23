ef1da2933a5c458b7975dadaa6bb15e9dea9afd2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-03-23 10:42:44 +0100

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name
+*
+* @name: parameter for name to be printed
+* @f: parameter for function pointer
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	return (f(name));
+}
