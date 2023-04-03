a0c737017824600de6607387c8f7a67d3887b639
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-04-03 16:12:45 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ *
+ * @head - pointer to head of linked list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	/*free head if NULL*/
+	if (head == NULL)
+		free(head);
+	else
+	{
+		while (head && head->next)
+		{
+			currentNode = head;
+			head = head->next;
+			free(currentNode->str);
+			free(currentNode);
+		}
+		free(head);
+	}
+}
