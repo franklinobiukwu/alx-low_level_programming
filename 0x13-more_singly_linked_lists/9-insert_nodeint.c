d76b79e220e766881858ad52b64965603fe02f2a
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-04-06 03:13:49 +0100

    function that inserts a new node at a given position FIX #1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,7 +47,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = NULL;
 	}
 	else
-		newNode = temp;
+		newNode->next = temp;
 
 	return (newNode);
 }
