9f3e82df6af1fec56d4689a42df9b3fba43660d0
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-04-10 13:50:08 +0100

    returns binary value at desired index FIX #1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,6 +13,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count;
 
+	/*return -1 if index is greater than bit size of unsigned long int*/
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1);
+	/*loop through binary value of the number to get to index*/
 	for (count = 0; count != index; count++)
 	{
 		if (n == 0 && count  < index)
