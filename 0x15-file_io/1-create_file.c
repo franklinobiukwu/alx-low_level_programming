ecae4cfca4118288cdb44db68c255229d7437272
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-04-28 03:40:14 +0100

    create file FIX #2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	/*create file with permissions*/
-	fd = open(filename, O_CREAT | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0600);
 	/*return -1 on creation failure*/
 	if (fd == -1)
 		return (-1);
