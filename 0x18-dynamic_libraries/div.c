b67fc6ea864bd460b3faa7b2c3a5c5c0bdf2feb6
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-06-20 00:18:06 +0100

    a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+
+div_t div(int a, int b);
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -24,4 +24,11 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+/*C functions in Python*/
+int add(int a, int b);
+int sub(int a, int b);
+div_t div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
 #endif
