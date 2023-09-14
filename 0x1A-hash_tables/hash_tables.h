62cf2cfbc5422424f2fda1d038d815dbbf5cfcb1
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-14 03:49:49 +0100

    function that creates a hash table

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,13 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*STRUCTS*/
+#include "structs.h"
+
+/*PROTOTYPES*/
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /*HASH_TABLES_H*/
